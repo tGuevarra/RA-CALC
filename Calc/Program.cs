e4fa459865ee4bc9596ff2e1178702a269b59867
tGuevarra <104074409+tGuevarra@users.noreply.github.com>
2022-04-27 14:33:21 +0700

    Initial commit

---
+++ Calc/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Calc
+{
+    public class Calculator
+    {
+        public static void Main()
+        {
+            var repeat = 'y';
+            while (repeat == 'y')
+            {
+                double firstValue;
+                double secondValue;
+                double finalValue;
+                char operation;
+
+                Console.WriteLine("Welcome to Calc!");
+                Console.WriteLine("\nChoose first number");
+                firstValue = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Choose second number");
+                secondValue = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("\nChoose your operation '+', '-', '*', '/'");
+                operation = Convert.ToChar(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case ('+'):
+                        finalValue = firstValue + secondValue;
+                        Console.WriteLine($"Sum {finalValue}");
+                        break;
+                    case ('-'):
+                        finalValue = firstValue - secondValue;
+                        Console.WriteLine($"Subtraction {finalValue}");
+                        break;
+                    case '*':
+                        finalValue = firstValue * secondValue;
+                        Console.WriteLine($"Multiply {finalValue}");
+                        break;
+                    case '/':
+                        finalValue = firstValue / secondValue;
+                        if (secondValue == 0) Console.WriteLine("U can't div on 0");
+                        else
+                        Console.WriteLine($"Division {finalValue}");
+                        break;
+                    default:
+                        Console.WriteLine("Choose the right nubmers or operation");
+                        break;
+               
+                }
+                Console.WriteLine("Do you want to continue? y/n");
+                repeat = Convert.ToChar(Console.ReadLine());
+            }
+
+        }
+    }
+}
